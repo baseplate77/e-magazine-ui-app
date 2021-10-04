dd4bfc67451054db6c66a6f75cd5339cb90f1830
baseplate77 <baseplate8087@gmail.com>
2021-10-04 23:07:52 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:e_magazine_ui/screen/magazine_detail/magazinbe_detail_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'core/theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,33 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Magazi',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Text("Magazi"),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      theme: AppTheme.light(),
+      home: const MagazineDetailScreen(),
     );
   }
 }
---
+++ lib/screen/magazine_detail/magazinbe_detail_screen.dart
@@ -0,0 +1,25 @@
+﻿import 'package:flutter/material.dart';
+
+class MagazineDetailScreen extends StatefulWidget {
+  const MagazineDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  _MagazineDetailScreenState createState() => _MagazineDetailScreenState();
+}
+
+class _MagazineDetailScreenState extends State<MagazineDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.grey,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
